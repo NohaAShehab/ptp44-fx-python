3f2792a68ad9905aa0b88bccfc377e4e4d73d2ff
Noha Shehab <nohashehab.iti@gmail.com>
2023-12-27 13:29:06 +0200

    introduction to programming using python

---
+++ intro_variables.py
@@ -0,0 +1,53 @@
+
+
+
+print("Welcome to ITI ")
+name = 'Noha'   # name variable is from type string
+age = 31   # age variable is from type integer
+year = "2024"
+happy = True
+sleepy = False
+g= 9.8
+
+track  ='fx'
+
+message=  'we support Al-Qasam'
+
+# print(message)
+# print(name)
+# print(age)
+# print(g)
+# print(track)
+#
+# print("Amira is the best in the track")
+#
+# print("message")
+#
+# # this is a comment
+#
+#
+# newmessage = '################################'
+# print(newmessage)
+#
+#
+# # print type of variables
+# print(type(name))
+# print(type(sleepy))
+# print(type(year))
+# print(type(age))
+#
+# t=type(age)
+# print(t)
+
+# print can output multiple results at the same time
+
+print(name, age,g)
+
+# ask user to enter value
+
+salary = input("please enter your salary: ")  # input function always returns with string
+# save user input into string
+
+print("salary is ", salary)
+
+print(type(salary))
