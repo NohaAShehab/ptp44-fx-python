cf4767b15c5bd774ac1c4444a953003b617fc237
Noha Shehab <nohashehab.iti@gmail.com>
2024-01-20 10:01:06 +0200

    introduction to functions

---
+++ functionss__.py
@@ -0,0 +1,152 @@
+#
+# course=  'python'
+# print("Ahmed", course)
+
+
+#
+# """ define a function"""
+# # def saygoodmorning ():
+# #     print("Good morning")
+# #     return
+#
+# def saygoodmorning ():
+#     print("Good morning")
+#
+# # calling the function
+# res=saygoodmorning()
+# print(res)
+#
+# saygoodmorning()
+#
+# saygoodmorning()
+#
+# saygoodmorning()
+#
+
+
+
+# print("10".isdigit())
+
+# # print(len())
+# print()
+
+
+
+""" write a function that prints hello from ghaza """
+""" function accepts zero arguments """
+
+# def helloGhaza():
+#     print("Hello From Ghaza")
+#
+#
+#
+# fun_res=helloGhaza()
+# print(fun_res)
+
+
+# helloGhaza("noha")
+
+
+# def helloGhaza():
+#     print("Hello From Ghaza")
+#     return
+#
+#
+#
+# fun_res=helloGhaza()
+# print(fun_res)
+
+
+#
+# """ write a function that asks the user to enter his/her name and print greating message for him/her"""
+#
+#
+# def greetuser():
+#     name = input("please enter your name: ")
+#     great_message =  f"Nice to meet you {name}"
+#     print(great_message)
+#
+#
+#
+# greetuser()
+
+
+
+""" write a function that asks the user to enter his/her name and print 
+greating message for him/her and return with name """
+
+
+# def greetuser():
+#     name = input("please enter your name: ")
+#     great_message =  f"Nice to meet you {name}"
+#     print(great_message)
+#     return name
+#
+#
+#
+# customer_name=greetuser()
+
+
+
+" function return more than one value "
+""" write a function that asks the user to enter his/her first name lastname then return with 
+firstname , lastname, fullname """
+
+# def get_full_name():
+#     fname= input("please enter first name: ")
+#     lanme = input("please enter last name: ")
+#     full_name = f'{fname} {lanme}'
+#     return fname, lanme, full_name  # tuple
+#
+#
+# myname = get_full_name()
+# print(myname)
+
+# def get_full_name():
+#     fname= input("please enter first name: ")
+#     lanme = input("please enter last name: ")
+#     full_name = f'{fname} {lanme}'
+#     return fname, lanme, full_name  # tuple
+#
+#
+# myname = get_full_name()
+# print(myname[0])
+
+
+""" function can accpet parameters """
+
+
+# def sumnum(num1, num2):
+#     res = num1 + num2
+#     print(f"from inside the function res = {res}")
+#     return res
+#
+#
+# sum_res= sumnum(10,20)
+#
+# print(sumnum(4,5))
+# print("------------")
+
+""" write a function that accept four numbers and return total, min, max of them """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
